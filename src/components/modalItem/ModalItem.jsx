4a2741ea789aa0417a795f1a9d3b5e777fef42c0
MarianoTorres <marianotorresdistefano@gmail.com>
2024-06-14 17:07:17 -0300

    Trabajos en Modal

--- src/components/modalItem/ModalItem.jsx
+++ src/components/modalItem/ModalItem.jsx
@@ -3,12 +3,17 @@ import { CgClose } from "react-icons/cg";
 
 export default function ModalItem({ item }) {
   let cadenaId = (item.name).replaceAll([" "], "-")
+
+  const closeModal = () => {
+    window.location.hash = '/';
+  };
+
   return (
     <div id={cadenaId} key={item.name} className={"modalContainer"}>
       <div className="modalItem">
         <div className='modalHeader'>
           <h4>{item.name}</h4>
-          <CgClose className='closeButton closeButton--xl' />
+          <CgClose className='closeButton closeButton--xl' onClick={closeModal} />
         </div>
 
         <div className='modalImg'>
